9250e5c2b6fc4219e3893d2ccb8519fed81c3f86
oslek222 <87049204+oslek222@users.noreply.github.com>
2021-07-29 15:54:45 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+console.log("script");
+
+var userName;
+userName="kevin";
+var userAge=34;
+var student=true;
+
+document.write(`
+<p> <b> Name:</b> ${userName}</p>
+<p> <b> Age: </b> ${userAge}</p>
+`);
+
+var userName="matthew";
+var userAge=34;
+
+document.write(`
+<p> <b> Name:</b> ${userName}</p>
+<p> <b> Age: </b> ${userAge}</p>
+`);
+
+let clientName="dude";
+let clientType="premium";
+
+clientName="Kenny";
+
+document.write(`
+<p> <b> Name:</b> ${userName}</p>
+<p> <b> Age: </b> ${userAge}</p>
+`);
+
+
+
+
+//console.log(userName);
+//console.log(userAge);
+//console.log(student);
+
+
+userName="tom";
+userName="harry";
+
+
+
